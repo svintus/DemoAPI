cce77bba67c07d359ef0284fa2d79b5739c3c95b
Ivan Sergeyenko <svintus@gmail.com>
2018-09-25 14:19:06 -0400

    Print json from request

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from app import app
+from flask import request, jsonify
 
-@app.route('/')
-@app.route('/index')
-def index():
+@app.route('/', methods=['GET'])
+def get():
     return "Hello, World!"
+
+@app.route('/', methods=['POST'])
+def post():
+    json = request.get_json()
+    print(json)
+    return ""
